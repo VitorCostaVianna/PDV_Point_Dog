9540670a0777d1317a5fb22a4c4c5d4947ca8ad9
VitorCostaVianna <vcvianna@sga.pucminas.br>
2026-01-14 19:20:44 -0300

    Add Produto entity and repository for product management

---
+++ src/main/java/point/dog/model/Produto.java
@@ -0,0 +1,25 @@
+package point.dog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Double preco;
+}
---
+++ src/main/java/point/dog/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package point.dog.repository;
+
+import point.dog.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
