42c5ab0e63ff8e73b4f98f7a2c525ef304df47aa
VitorCostaVianna <vcvianna@sga.pucminas.br>
2026-01-14 17:34:46 -0300

    first commit

---
+++ src/main/java/point/dog/SistemaGerenciamentoPointDogApplication.java
@@ -0,0 +1,13 @@
+package point.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoPointDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoPointDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/point/dog/SistemaGerenciamentoPointDogApplicationTests.java
@@ -0,0 +1,13 @@
+package point.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoPointDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
